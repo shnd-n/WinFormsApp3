76be1e6c0b0f150b02f7a9005ca949d997233b1a
종찬 임 <23017076@office.suwon.ac.kr>
2026-03-05 13:08:41 +0900

    배경색 범위를 255에서 250으로 변경함

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace WinFormsApp3
         private void button1_Click(object sender, EventArgs e)
         {
             Random rd = new Random();// 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
-            this.BackColor= Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+            this.BackColor= Color.FromArgb(rd.Next(250), rd.Next(250), rd.Next(256));
         }
     }
 }

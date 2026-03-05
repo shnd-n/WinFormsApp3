746ade00f6e2a66f2ba01722579d3270ef3eacba
user <user@DESKTOP-FUGTPVF>
2026-03-05 12:51:30 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Label label1;
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label1.Location = new Point(275, 60);
+            label1.MaximumSize = new Size(100, 100);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 32);
+            label1.TabIndex = 1;
+            label1.Text = "임종찬";
+            label1.Click += label1_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = Properties.Resources.emblem_01;
+            pictureBox1.Location = new Point(12, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(226, 226);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.Location = new Point(275, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(179, 30);
+            label2.TabIndex = 2;
+            label2.Text = "수원대 컴퓨터SW";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label3.Location = new Point(275, 206);
+            label3.Name = "label3";
+            label3.Size = new Size(168, 32);
+            label3.TabIndex = 3;
+            label3.Text = "01096847411";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(525, 278);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "랜덤색변경";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Goldenrod;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "나의 첫 프로그램";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();// 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
+            this.BackColor= Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+    }
+}

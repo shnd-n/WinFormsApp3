035a35cde70e79e963e27c79f4d77a715ff87b48
Jongchan Lim <Jongchan0517@naver.com>
2026-03-10 21:36:49 +0900

    - 이메일 추가 - github 연결 버튼 추가 - 사진 변경 기능 추

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             label2 = new Label();
             label3 = new Label();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -64,31 +66,54 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label2.Location = new Point(275, 144);
+            label2.Location = new Point(275, 111);
             label2.Name = "label2";
-            label2.Size = new Size(179, 30);
+            label2.Size = new Size(109, 30);
             label2.TabIndex = 2;
-            label2.Text = "수원대 컴퓨터SW";
+            label2.Text = "컴퓨터SW";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label3.Location = new Point(275, 206);
+            label3.Location = new Point(275, 156);
             label3.Name = "label3";
-            label3.Size = new Size(168, 32);
+            label3.Size = new Size(318, 32);
             label3.TabIndex = 3;
-            label3.Text = "01096847411";
+            label3.Text = "jongchan0517@naver.com";
             // 
             // button1
             // 
-            button1.Location = new Point(525, 278);
+            button1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(607, 332);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(132, 83);
             button1.TabIndex = 4;
             button1.Text = "랜덤색변경";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.Location = new Point(321, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 50);
+            button2.TabIndex = 5;
+            button2.Text = "github";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.Location = new Point(70, 300);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 49);
+            button3.TabIndex = 6;
+            button3.Text = "사진변경";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
@@ -96,6 +121,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Goldenrod;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -115,5 +142,7 @@
         private Label label2;
         private Label label3;
         private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,21 @@ namespace WinFormsApp3
         private void button1_Click(object sender, EventArgs e)
         {
             Random rd = new Random();// 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
-            this.BackColor= Color.FromArgb(rd.Next(250), rd.Next(250), rd.Next(256));
+            this.BackColor = Color.FromArgb(rd.Next(250), rd.Next(250), rd.Next(256));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/shnd-n",
+                UseShellExecute = true
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.수원대7;
         }
     }
 }
